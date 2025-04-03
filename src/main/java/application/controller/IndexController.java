c4a39b07be91ebe534a4af7b1499185b357856c6
CaioCamargo2005 <163781046+CaioCamargo2005@users.noreply.github.com>
2025-04-03 21:57:12 +0000

    Genero 03/04/2025

--- src/main/java/application/controller/IndexController.java
+++ src/main/java/application/controller/IndexController.java
@@ -1,12 +1,94 @@
 package application.controller;
 
+import java.util.Optional;
+
+
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.ui.Model;
 import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestMethod;
+import org.springframework.web.bind.annotation.RequestParam;
+
+import application.model.Genero;
+import application.repository.GeneroRepository;
+
+
 
 @Controller
 public class IndexController {
-    @RequestMapping("/")
+    @Autowired
+    private GeneroRepository database;
+
+    @RequestMapping(value = "/")
     public String home() {
-        return "home";
+        return "list";
+    }
+
+    @RequestMapping(value = {"/list", ""})
+    public String select(Model ui){
+        ui.addAttribute("registros", database.findAll());
+        return "list";
+    }
+
+    @RequestMapping("/insert")
+    public String insert(){
+        return "formInsert";
+    }
+
+    @RequestMapping(value = "/insert", method = RequestMethod.POST)
+    public String insert(@RequestParam("descricao") String descricao) {
+        Genero data = new Genero();
+        data.setNome(descricao);
+
+        database.save(data);
+
+        return "redirect:/list";
+    }
+
+    @RequestMapping("/update")
+    public String update(@RequestParam("id") long id, Model ui){
+        Optional<Genero> resultado = database.findById(id);
+        
+        if(resultado.isPresent()) {
+            ui.addAttribute("registros", resultado.get());
+            return "formUpdate";
+        }
+        
+        return "redirect:/list";
+    }
+
+    @RequestMapping(value = "/update", method = RequestMethod.POST)
+    public String update(
+    @RequestParam("id") long id,
+    @RequestParam("nome") String descricao){
+        Optional<Genero> resultado = database.findById(id);
+
+        if(resultado.isPresent()) {
+            resultado.get().setNome(descricao);
+
+            database.save(resultado.get());
+        }
+        
+        return "redirect:/list";
+    }
+
+    @RequestMapping(value = "/delete")
+    public String delete(@RequestParam("id") long id, Model ui){
+    Optional<Genero> resultado = database.findById(id);
+
+    if(resultado.isPresent()) {
+        ui.addAttribute("registros", resultado.get());
+        return "formDelete";
+    }
+    
+    return "redirect:/list";
+    }
+
+    @RequestMapping(value = "/delete", method = RequestMethod.POST)
+    public String delete(@RequestParam("id") long id){
+        database.deleteById(id);
+        return "redirect:/list";
     }
 }
--- src/main/java/application/model/Genero.java
+++ src/main/java/application/model/Genero.java
@@ -1,34 +1,31 @@
 package application.model;
 
-
 import jakarta.persistence.Entity;
 import jakarta.persistence.GeneratedValue;
 import jakarta.persistence.GenerationType;
 import jakarta.persistence.Id;
+import jakarta.persistence.Table;
 
 @Entity
+@Table(name = "genero")
 public class Genero {
-    
     @Id
     @GeneratedValue(strategy = GenerationType.IDENTITY)
-    private Long id;
+    private long id;
     private String nome;
 
-    // Getters and Setters
-    public Long getId() {
-        return id;
-    }
-
-    public void setId(Long id) {
+    public void setId(long id){
         this.id = id;
     }
-
-    public String getNome() {
-        return nome;
+    public long getId(){
+        return this.id;
     }
 
     public void setNome(String nome) {
         this.nome = nome;
     }
-    
+
+    public String getNome() {
+        return nome;
+    }
 }
